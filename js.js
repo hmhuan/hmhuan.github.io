99e9bf9317d6b48cb779fc3337abbaab68dd4e66
Huynh Minh Huan <minhhuanhuynh289@gmail.com>
2018-08-07 17:33:38 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
 var today = new Date();
 var year = today.getFullYear();
-var el = document.getElementById('footer');
-el.innerHTML = '<p>Copyright &copy;' + year+ '</p>';
+var el = document.getElementById("footer");
+el.innerHTML = "<p>Copyright &copy;" + year + " hmhuan All Rights Reserved</p>";
